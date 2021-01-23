c3f728f978c46064247f6819914320638ee35a81
siva <sivakrishnavegi@yahoo.com>
2021-01-23 16:36:27 +0530

    added news api logi

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,5 +1,19 @@
+require("dotenv").config()
+const axios = require('axios')
 const getNews = async (req, res, next) => { 
-    res.json({message: 'homepage routes'});
+let newsdata = []
+    await axios.get(process.env.REACT_APP_API_URL)
+    .then(res => {
+    
+        newsdata = res.data.articles
+        res.json({ news : newsdata })
+        
+    })
+    .catch(err => {
+        console.error(err); 
+    })
+    
+    res.json({message: newsdata});
 }
 
 exports.getNews =  getNews;
